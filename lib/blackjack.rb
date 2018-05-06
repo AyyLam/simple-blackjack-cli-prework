2db1a00e8d2022cd54f3e9f70e86eb5a69141416
Anthony Lam <al3101@nyu.edu>
2018-05-06 01:22:03 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,8 @@ def runner
   welcome 
   card_total = initial_round
   until card_total > 21
-    card_total += hit?(card_total)
+    new_total = hit?(card_total)
+    card_total += new_total
     display_card_total(card_total)
   end 
   end_game(card_total)
