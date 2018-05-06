dacda38a829e61b0a16150b1859e71094bc473cb
Anthony Lam <al3101@nyu.edu>
2018-05-06 01:13:37 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,8 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
   if input == 'h'
-    card_total += deal_card
+    card = deal_card
+    card_total += card
   elsif input != 's'
     invalid_command
   end
